8ec61ee08c6f80b468a97845d16deb8c5e7781f9
githubren <970910609@qq.com>
2020-02-11 14:45:32 +0800

    intent activity

---
+++ app/src/main/java/com/example/computer_ren/otherapplication/IntentActivity.java
@@ -0,0 +1,23 @@
+package com.example.computer_ren.otherapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+public class IntentActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+
+        TextView textView = findViewById(R.id.show_message_tv);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            textView.setText(intent.getStringExtra("data"));
+        }
+    }
+}
